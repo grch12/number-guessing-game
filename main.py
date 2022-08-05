3460b85d97d337a8f25c332c32f7fb031a2987a1
graphics123 <109024519+grch12@users.noreply.github.com>
2022-08-05 09:04:39 +0000

    加入异常处理功能

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import time
 import sys
 
 #判断参数数量
-if(len(sys.argv)  < 2):
+if (len(sys.argv) < 2):
     print('您没有提供次数限制。')
     exit(1)
 
 #限制次数
-guess_limit = int(sys.argv[1])
+try:
+    guess_limit = int(sys.argv[1])
+except ValueError:
+    print("给定的参数必须是整数。")
+    exit(1)
 
 cont = True
 scores = []
@@ -22,24 +26,28 @@ while cont:
 
     #开始判断_____________________
     for i in range(guess_limit):
-        guess = int(input('这个数在0到100之间，请猜：'))
-        if(guess == answer):
+        try:
+            guess = int(input('这个数在0到100之间，请猜：'))
+        except ValueError:
+            print('输入值不能被转换为整数型，请检查是否有输入错误。')
+            exit(1)
+        if (guess == answer):
             is_right = True
             break
-    
-        if(guess < answer):
+
+        if (guess < answer):
             print('你猜小了', end='，')
         else:
             print('你猜大了', end='，')
-    
-        if(i != guess_limit - 1):
+
+        if (i != guess_limit - 1):
             print('请继续猜。')
     #结束判断_____________________
 
     end_time = time.time()
     used_time = round(end_time - begin_time, 1)
 
-    if(not(is_right)):
+    if (not (is_right)):
         print('你已经用完了所有机会', end='，')
     else:
         print('你猜对了', end='，')
@@ -52,16 +60,16 @@ while cont:
     print('-----------------')
 
 #过滤列表
-filted =  list(filter(lambda x: x[1], scores))
-if(len(filted) != 0):
-    best_score = min(filted, key=lambda x:x[2])[2]
+filted = list(filter(lambda x: x[1], scores))
+if (len(filted) != 0):
+    best_score = min(filted, key=lambda x: x[2])[2]
 else:
     best_score = 0
 
 #显示成绩
 for number, won, seconds in scores:
     won_text = '胜利' if won else '失败'
-    label =  ''
-    if(won and seconds == best_score):
+    label = ''
+    if (won and seconds == best_score):
         label = '[最佳]'
     print(f'第{number}局 {won_text} 用时{seconds}{label}')
